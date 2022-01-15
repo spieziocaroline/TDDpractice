b964741567f670d40f285c5bca4fb27e974a3695
Caroline Spiezio <spieziocaroline@gmail.com>
2022-01-15 16:57:51 -0500

    first test spec created & passed&

---
+++ wrap.js
@@ -0,0 +1,5 @@
+const wrap = (line, maxLen) => {
+  if (line.length < 1) return line;
+};
+
+module.exports = wrap;
---
+++ wrap.spec.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('wrap', () => {
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap('', 10)).to.equal('');
+  });
+});
+
+/*we want to check that function is ..
+    returning a string
+    in which each line isn't longer than the column number
+    that the length of the string itself hasn't been changed
+*/
+
+/*possible cases
+  function isn't returning a string
+  function is passed an input that isn't a string
+  function returns a string but it isn't the same string that it was passed
+  function returns a string but at least one line of that string is too long,
+      aka its length is > than column's max
+  function gets a string that is less than column length to begin with
+  column length is 1
+  column length is 0
+  string length is 0
+*/
